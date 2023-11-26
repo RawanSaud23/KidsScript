d35be0233148ca3749b27db219621993d771f642
SaraKSSU <sarasyo2003@gmail.com>
2023-11-26 21:47:58 +0300

    DARK-MODE!

--- index/darkMode.js
+++ index/darkMode.js
@@ -1,6 +1,6 @@
  
  
- 
+ /*
   var icon = document. getElementById ("icon");
   icon.onclick = function () {
   document. body.classList. toggle ("dark-theme");
@@ -16,4 +16,41 @@
   icon.src = "images/moon.png";
   }
 }
-   
+   */
+
+ 
+function toggleDarkTheme() {
+  var body = document.body;
+  body.classList.toggle("dark-theme");
+
+ 
+  var icon = document.getElementById("icon");
+  if (body.classList.contains("dark-theme")) {
+    icon.src = "images/sun.png";
+  } else {
+    icon.src = "images/moon.png";
+  }
+
+ 
+  var isDarkThemeActive = body.classList.contains("dark-theme");
+  localStorage.setItem("isDarkThemeActive", isDarkThemeActive.toString());
+}
+
+ 
+document.addEventListener("DOMContentLoaded", function () {
+  var isDarkThemeActive = localStorage.getItem("isDarkThemeActive");
+  var body = document.body;
+  var icon = document.getElementById("icon");
+
+  if (isDarkThemeActive === "true") {
+    body.classList.add("dark-theme");
+    icon.src = "images/sun.png";
+  } else {
+    body.classList.remove("dark-theme");
+    icon.src = "images/moon.png";
+  }
+});
+
+ 
+var icon = document.getElementById("icon");
+icon.onclick = toggleDarkTheme;
